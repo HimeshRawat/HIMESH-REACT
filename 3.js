b1fe42dccc1563a77cf3a9494033f96602a10e9d
Abhishek Rawat <abrawatji66@outlook.com>
2024-07-01 00:53:02 +0530

    first react code

---
+++ 3.js
@@ -0,0 +1,3 @@
+const heading = React.createElement("h1", {}, "HELLO HIMESH JI");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
---
+++ 8.js
@@ -0,0 +1,13 @@
+const parent = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "HELLO JI kya hal hai apkeee"),
+    React.createElement("h2", {}, "HELLO HIMESH"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "HELLO JI kya hal hai apkeee"),
+    React.createElement("h2", {}, "HELLO HIMESH"),
+  ]),
+]);
+console.log(parent);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
